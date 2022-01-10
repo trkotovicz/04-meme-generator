db29421d2bf76318593a8bf33b6238afc75fa081
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-01-10 01:50:56 -0300

    Finalizando projeto. Itens 1 e 2 dando erro

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ let inputText = document.getElementById("text-input");
 let memeText = document.getElementById("meme-text");
 let btnInput = document.getElementById("btn-input");
 let inputImage = document.getElementById("meme-insert");
-let btnImage = document.getElementById("btn-image");
 let imageContainer = document.getElementById("meme-image-container");
+let memeImage = document.getElementById("meme-image");
 
-
+// add input texto
 btnInput.addEventListener("click", function() {
     if (inputText.value.length > 0) {
         memeText.innerText = inputText.value;
@@ -14,10 +14,20 @@ btnInput.addEventListener("click", function() {
     };
 });
 
-btnImage.addEventListener("click", function() {
-   
-});
 
+// add input img
+/* Referência tirada do MDN
+https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example.3a_using_object_urls_to_display_images
+*/
+inputImage.addEventListener("change", addImage);
+// usar o evento "change" foi sugerido em alguma mentoria (não lembro qual)
+
+function addImage(event) {
+    memeImage.src = URL.createObjectURL(event.target.files[0]);
+};
+
+
+// add moldura ao clicar nos botões
 let fireBtn = document.getElementById("fire");
 fireBtn.addEventListener("click", function() {
     imageContainer.style.border = "3px dashed red";
@@ -32,3 +42,22 @@ let earthBtn = document.getElementById("earth");
 earthBtn.addEventListener("click", function() {
     imageContainer.style.border = "6px groove green";
 });
+
+
+// add meme pré-selecionado
+let meme1 = document.getElementById("meme-1");
+meme1.addEventListener("click", famousMeme);
+
+let meme2 = document.getElementById("meme-2");
+meme2.addEventListener("click", famousMeme);
+
+let meme3 = document.getElementById("meme-3");
+meme3.addEventListener("click", famousMeme);
+
+let meme4 = document.getElementById("meme-4");
+meme4.addEventListener("click", famousMeme);
+
+// função que busca a source da imagem clicada (event) e adiciona na source da imagem id="meme-image"
+function famousMeme(event) {
+    memeImage.src = event.target.src;
+};
