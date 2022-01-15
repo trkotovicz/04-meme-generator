6d9268f8b09b45109622cf6bc316dcc1aa07ca1f
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-01-14 22:08:02 -0300

    Melhora Css e tira erros de lint

--- script.js
+++ script.js
@@ -1,61 +1,58 @@
-let inputText = document.getElementById("text-input");
-let memeText = document.getElementById("meme-text");
-let btnInput = document.getElementById("btn-input");
-let inputImage = document.getElementById("meme-insert");
-let imageContainer = document.getElementById("meme-image-container");
-let memeImage = document.getElementById("meme-image");
+const inputText = document.getElementById('text-input');
+const memeText = document.getElementById('meme-text');
+const inputImage = document.getElementById('meme-insert');
+const imageContainer = document.getElementById('meme-image-container');
+const memeImage = document.getElementById('meme-image');
 
 // add input texto
-inputText.addEventListener("keyup", function() {
-    if (inputText.value.length > 0) {
-        memeText.innerText = inputText.value;
-    };
+inputText.addEventListener('keyup', () => {
+  if (inputText.value.length > 0) {
+    memeText.innerText = inputText.value;
+  }
 });
-
 
 // add input img
 /* Referência tirada do MDN
 https://developer.mozilla.org/en-US/docs/Web/API/File/Using_files_from_web_applications#example.3a_using_object_urls_to_display_images
 */
-inputImage.addEventListener("change", addImage);
-// usar o evento "change" foi sugerido em alguma mentoria (não lembro qual)
 
 function addImage(event) {
-    memeImage.src = URL.createObjectURL(event.target.files[0]);
-};
+  memeImage.src = URL.createObjectURL(event.target.files[0]);
+}
 
+inputImage.addEventListener('change', addImage);
+// usar o evento "change" foi sugerido em alguma mentoria (não lembro qual)
 
 // add moldura ao clicar nos botões
-let fireBtn = document.getElementById("fire");
-fireBtn.addEventListener("click", function() {
-    imageContainer.style.border = "3px dashed red";
+const fireBtn = document.getElementById('fire');
+fireBtn.addEventListener('click', () => {
+  imageContainer.style.border = '3px dashed red';
 });
 
-let waterBtn = document.getElementById("water");
-waterBtn.addEventListener("click", function() {
-    imageContainer.style.border = "5px double blue";
+const waterBtn = document.getElementById('water');
+waterBtn.addEventListener('click', () => {
+  imageContainer.style.border = '5px double blue';
 });
 
-let earthBtn = document.getElementById("earth");
-earthBtn.addEventListener("click", function() {
-    imageContainer.style.border = "6px groove green";
+const earthBtn = document.getElementById('earth');
+earthBtn.addEventListener('click', () => {
+  imageContainer.style.border = '6px groove green';
 });
-
-
-// add meme pré-selecionado
-let meme1 = document.getElementById("meme-1");
-meme1.addEventListener("click", famousMeme);
-
-let meme2 = document.getElementById("meme-2");
-meme2.addEventListener("click", famousMeme);
-
-let meme3 = document.getElementById("meme-3");
-meme3.addEventListener("click", famousMeme);
-
-let meme4 = document.getElementById("meme-4");
-meme4.addEventListener("click", famousMeme);
 
 // função que busca a source da imagem clicada (event) e adiciona na source da imagem id="meme-image"
 function famousMeme(event) {
-    memeImage.src = event.target.src;
-};
+  memeImage.src = event.target.src;
+}
+
+// add meme pré-selecionado
+const meme1 = document.getElementById('meme-1');
+meme1.addEventListener('click', famousMeme);
+
+const meme2 = document.getElementById('meme-2');
+meme2.addEventListener('click', famousMeme);
+
+const meme3 = document.getElementById('meme-3');
+meme3.addEventListener('click', famousMeme);
+
+const meme4 = document.getElementById('meme-4');
+meme4.addEventListener('click', famousMeme);
